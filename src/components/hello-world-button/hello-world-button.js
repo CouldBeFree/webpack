9136211158305fcf360322c34937dadab48a3bb0
couldbefree <zoriaalexandr@gmail.com>
2020-05-13 20:54:35 +0300

    styles

--- src/components/hello-world-button/hello-world-button.js
+++ src/components/hello-world-button/hello-world-button.js
@@ -1,8 +1,17 @@
+import './hello-world-button.css';
+
 class helloWorldButton {
 	render() {
 		const button = document.createElement('button');
 		button.innerHTML = 'Hello world';
+		button.classList.add('hello-world-button');
 		const body = document.querySelector('body');
+		button.onclick = function () {
+			const p = document.createElement('p');
+			p.innerHTML = 'Hello world';
+			p.classList.add('hello-world-text');
+			body.appendChild(p);
+		}
 		body.appendChild(button);
 	}
 }
