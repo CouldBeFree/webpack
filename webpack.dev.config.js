79ee87d6bdf17583ed1d236f862021c1a1b39e19
couldbefree <zoriaalexandr@gmail.com>
2020-05-16 14:04:50 +0300

    changes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin }   = require('clean-webpack-plugin');
 const HtmlWebpackPlugin        = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/hello-world.js',
+  entry: {
+    'hello-world': './src/hello-world.js',
+    'kiwi': './src/kiwi.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, './dist'),
     publicPath: ''
   },
@@ -57,9 +60,18 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
+      filename: 'hello-world.html',
+      chunks: ['hello-world'],
       title: 'Webpack course',
       template: 'src/page-template.hbs',
       description: 'Some description'
+    }),
+    new HtmlWebpackPlugin({
+      filename: "kiwi.html",
+      title: 'Webpack course',
+      chunks: ['kiwi'],
+      template: 'src/page-template.hbs',
+      description: 'Kiwi'
     })
   ]
 };
